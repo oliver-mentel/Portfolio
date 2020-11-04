8d43bef812046c1de0f1933ffa924b6971a57d53
Oliver Mentel <oliver.mentel@yahoo.de>
2020-11-04 18:42:43 +0100

    Added About Me and Carousel

--- website/src/app/about-me/about-me.component.ts
+++ website/src/app/about-me/about-me.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { CarouselConfig } from 'ngx-bootstrap/carousel';
+
+
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
-  styleUrls: ['./about-me.component.scss']
+  styleUrls: ['./about-me.component.scss'],
+  providers : [
+    { provide: CarouselConfig, useValue: { noPause: false, showIndicators: true } }
+  ]
 })
 export class AboutMeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit(): void {
   }
--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -6,6 +6,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 
 
@@ -14,12 +19,17 @@ import { AboutMeComponent } from './about-me/about-me.component';
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    AboutMeComponent
+    AboutMeComponent,
+    ProjectsComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CarouselModule.forRoot()
+
 
   ],
   providers: [],
